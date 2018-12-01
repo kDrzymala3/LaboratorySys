7b2d2815c0d0b4802af9ab2a7b4b914030424414
drzymek <kacper.drzymala333@gmail.com>
2018-12-01 14:06:26 +0100

    init

---
+++ src/main/java/com/pl/kd/LaboratorySysApplication.java
@@ -0,0 +1,12 @@
+package com.pl.kd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorySysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaboratorySysApplication.class, args);
+    }
+}
